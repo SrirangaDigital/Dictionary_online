98fcb9ee23d33d5ee3aa85b3025fe0dc30ac6b7c
shivasdst <shivably04sdst@gmail.com>
2014-11-28 15:41:41 +0530

    Commit No. 13 - preprocess.pl is modified

--- src/preprocess.pl
+++ src/preprocess.pl
@@ -2,13 +2,67 @@
 
 $file = "letter_x.tex";
 $output = "x_uni.html";
+$pictfile = "x_figs_list.tex";
+$indexfile = "indexofletterx.tex";
 
 open(IN, "$file") or die "Can't open $file";
 open(OUT, ">$output") or die "Can't open $output";
+open(IDX, "$indexfile") or die "Can't open $indexfile";
+
+@indexlist = <IDX>;
+
+close(IDX);
+
 
 $line = <IN>;
 $xid = 0;
 
+
+$preamble = "<!doctype html>
+<html lang=\"en\" class=\"no-js\">
+<head>
+	<meta charset=\"UTF-8\">
+	<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+	<link rel=\"stylesheet\" href=\"../css/reset.css\"> <!-- CSS reset -->
+	<link rel=\"stylesheet\" href=\"../css/style.css\"> <!-- Resource style -->
+	<script type=\"text/x-mathjax-config\">
+	  MathJax.Hub.Config({
+		tex2jax: {inlineMath: [[\"\$\",\"\$\"],[\"\\\\(\",\"\\\\)\"]]}
+	  });
+	</script>
+	<script type=\"text/javascript\" src=\"../MathJax/MathJax.js?config=TeX-AMS_HTML-full\"></script>
+	<title>Univerity of Mysore - English Kannada Dictionary</title>
+</head>
+<body>
+	<header class=\"cd-header\">
+		<div id=\"cd-logo\">
+			<a href=\"#0\">
+				<img src=\"img/logo.png\" alt=\"Logo\">
+				<span>UNIVERSITY OF MYSORE</span>
+			</a>
+		</div>
+
+		<nav class=\"cd-main-nav\">
+			<ul>
+				<li><a href=\"#0\">Home</a></li>
+				<li><a href=\"#0\">Dictionary</a></li>
+			</ul>
+		</nav> <!-- cd-main-nav -->
+	</header>
+
+	<main class=\"cd-main-content\">
+		<div class=\"cd-scrolling-bg cd-color-2\">
+			<div class=\"cd-container\">
+				<h1 class=\"clr1\">English - Kannada Dictionary</h1>
+                <h2>ಇಂಗ್ಲಿಷ್ - ಕನ್ನಡ ನಿಘಂಟು</h2>";
+
+$post = "			</div> <!-- cd-container -->
+		</div> <!-- cd-scrolling-bg -->
+	</main> <!-- cd-main-content -->
+</body>
+</html>";
+
+print OUT $preamble . "\n";
 
 while($line)
 {
@@ -19,7 +73,8 @@ while($line)
 	$line =~ s/\\kern1pt//g;
 	$line =~ s/\\quad//g;
 	$line =~ s/[\s]+/ /g;
-	$line =~ s/\\hyperlink\{(.*)\}\{\\raisebox\{(.*)\}\[(.*)\]\[(.*)\]\{\\pdfimage(.*)\{([A-Z]_Pictures)\/(.*).jpg}}}/\6\/\7.jpg/g;
+
+	#$line =~ s/\\hyperlink\{(.*)\}\{\\raisebox\{(.*)\}\[(.*)\]\[(.*)\]\{\\pdfimage(.*)\{([A-Z]_Pictures)\/(.*).jpg}}}/\6\/\7.jpg/g;
 	
 	if($line =~ /\\bentry/)
 	{
@@ -32,26 +87,49 @@ while($line)
 	}
 	elsif($line =~ /\\word\{(.*)\}/)
 	{
-		print OUT "<span class=\"engWord clr1\">".  $1 ."</span>\n";
+		print OUT "<div class=\"whead\" id=\"xid$xid\">\n";
+			print OUT "\t<span class=\"engWord clr1\">".  $1 ."</span>\n";
 	}
 	elsif($line =~ /\\word\[(.*)\]\{(.*)\}/)
 	{
-		print OUT "<span class=\"engWord clr1\">".  $2 ."</span>\n";
+		print OUT "<div class=\"whead\" id=\"xid$xid\">\n";
+			print OUT "\t<span class=\"engWord clr1\">".  $2 ."</span>\n";
 	}
 	elsif($line =~ /\\wordwithhyphen\{(.*)\}\{(.*)\}/)
 	{
-		print OUT "<span class=\"engWord clr1\">".  $2 ."</span>\n";
+		print OUT "<div class=\"whead\" id=\"xid$xid\">\n";	
+			print OUT "\t<span class=\"engWord clr1\">".  $2 ."</span>\n";
 	}	
 	elsif($line =~ /\\wordnospeech\{(.*)\}\{(.*)\}/)
 	{
-		print OUT "<span class=\"engWord clr1\">".  $2 ."</span>\n";
-	}	
+		print OUT "<div class=\"whead\" id=\"xid$xid\">\n";			
+			print OUT "\t<span class=\"engWord clr1\">".  $2 ."</span>\n";
+	}
 	elsif($line =~ /\\pron\{(.*)\}/)
 	{
 		$pron = $1;
 		if($pron ne "?")
+		{			
+			if($pron =~ /Z|Yx/)
+			{
+				if(!(-e "X/pronunciation/xid$xid.png"))
+				{
+					gen_png($pron);
+				}
+				$pron =~ s/Z//g;
+				$pron =~ s/Yx/yx/g;
+				$pront =~ s/\\kern1pt//g;
+				
+				print OUT "\t<span class=\"kanWord\"><img src=\"X/pronunciation/xid". $xid . ".png\" alt=\"". gen_unicode($pron) ."\" /></span><br />\n";
+			}
+			else
+			{
+				print OUT "\t<span class=\"kanWord\">". gen_unicode($pron) ."</span><br />\n";
+			}
+		}
+		else
 		{
-			print OUT "<span class=\"kanWord\">". gen_png($pron) ."</span>\n";
+			print OUT "\t<span class=\"kanWord\"></span><br />\n";
 		}
 		#print OUT "<span class=\"kanWord\">". gen_unicode($1) ."</span>\n";
 	}
@@ -59,18 +137,255 @@ while($line)
 	{
 		$gl = preprocess($1);
 		$gl = gen_unicode($gl);
-		print OUT "<span class=\"grammarLabel\">". $gl ."</span>\n";
+		print OUT "\t<span class=\"grammarLabel\">". $gl ."</span>\n";
+		print OUT "</div>\n";
+	}
+	elsif($line =~ /\\bmng/)
+	{
+		print OUT "<div class=\"wBody\">\n";	
+	}
+	elsif($line =~ /\\emng/)
+	{
+		print OUT "</div>\n";	
+	}
+	elsif($line =~ /\\bnum/)
+	{
+		print OUT "<ol>\n";	
+	}
+	elsif($line =~ /\\enum/)
+	{
+		print OUT "</ol>\n";	
+	}
+	elsif($line =~ /\\banum/)
+	{
+		print OUT "<ol class=\"alph\">\n";	
+	}
+	elsif($line =~ /\\eanum/)
+	{
+		print OUT "</ol>\n";	
+	}
+	elsif($line =~ /\\num\{[0-9]+\}(.*)/)
+	{
+		$numline = preprocess($1);
+		$numline = gen_unicode($numline);
+		print OUT "<li>". $numline  ."</li>\n";
+	}
+	elsif($line =~ /\\alnum\{[a-z]\}(.*)/)
+	{
+		$numline = preprocess($1);
+		$numline = gen_unicode($numline);
+		print OUT "<li>". $numline  ."</li>\n";
+	}
+	elsif($line =~ /^%/)
+	{
+		
 	}
 	else
 	{
 		$line = preprocess($line);
 		$line = gen_unicode($line);
-		print OUT $line . "\n";		
+		print OUT "<p>" . $line . "</p>\n";		
 	}
 	
 	$line = <IN>;	
 }
 
+
+output_pictures();
+
+print OUT $post;
+
+close(IN);
+close(OUT);
+
+sub output_pictures()
+{
+	open(FIGS, "$pictfile") or die "Can't open $pictfile\n";
+	
+	$pictline = <FIGS>;
+	
+	print OUT "<div class=\"images_list\">\n";
+	
+	while($pictline)
+	{
+		chop($pictline);
+		
+		if($pictline =~ /\\hypertarget\{(.*)\}\{\}/)
+		{			
+			$pictid = $1;
+			print OUT "\t<div class=\"img_display\" id=\"$pictid\">\n";
+				
+		}
+		elsif($pictline =~ /\\pdfimage\{([A-Z])_Pictures\/(.*)\.jpg\}\}\\hfill\}/)
+		{
+			$pictalpha = $1;
+			$pictname = $2;
+			print OUT "\t\t<div><img src=\"$pictalpha/Pictures/$pictname.jpg\" alt=\"$pictname\"/></div>\n";
+		}
+		elsif($pictline =~ /\\caption\{\\eng\{(.*)\}\}/)
+		{
+			$caption = $1;
+			print OUT "\t\t<div class=\"fig_caption\">$caption</div>\n";
+			print OUT "\t</div>\n";
+		}
+	
+		$pictline = <FIGS>;
+	}
+	
+	print OUT "</div>\n";
+	close(FIGS);
+}
+
+sub gen_unicode()
+{	
+	my($kan_str) = @_;
+	open(TMP, ">tmp.txt") or die "Can't open tmp.txt\n";
+	my ($tmp,$flg,$i,$endash_uni,$endash,$flag);
+	$flg = 1;
+
+	$kan_str =~ s/\\bf//g;
+	$kan_str =~ s/\\num\{(.*?)\}//g;
+	$kan_str =~ s/\\ralign\{(.*?)\}/!E! $ralign_btag !K! $1 !E! $ralign_etag !K! /g;
+	$kan_str =~ s/\\char'263/!E!&#x0CBD;!K!/g;
+	$kan_str =~ s/\\char'365/!E!&#x0CC4;!K!/g;
+	$kan_str =~ s/\\char'273/!E!&#x0CB1;!K!/g;
+	$kan_str =~ s/\\s /!E!&#x0CBD;!K!/g;
+	$kan_str =~ s/RV/VR/g;
+	$kan_str =~ s/qq/q/g;
+	$kan_str =~ s/Ryx/yxR/g;
+	$kan_str =~ s/RyX/yxR/g;
+	$kan_str =~ s/Rq/qR/g;
+	$kan_str =~ s/RY/YR/g;
+	$kan_str =~ s/\\cdots/!E!&#x2026;!K!/g;
+
+	$flag = 1;
+	while($flag)
+	{
+		#print "HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH\n";
+		if($kan_str =~ /\{\\rm (.*?)\}/)
+		{
+			$kan_str =~ s/\{\\rm (.*?)\}/!E!\1!K!/;
+		}
+		elsif($kan_str =~ /\\eng\{(.*?)\}/)
+		{
+			$kan_str =~ s/\\eng\{(.*?)\}/!E!\1!K!/;
+		}
+		elsif($kan_str =~ /\\imglink\{(.*)\}\{\\raisebox(.*)\{([A-Z])_Pictures\/(.*)\.jpg\}\}\}/)
+		{
+			$kan_str =~ s/\\imglink\{(.*?)\}\{\\raisebox(.*?)\{([A-Z])_Pictures\/(.*?)\.jpg\}\}\}/!E!<span class="crossref"><a href="#\4fig">Figure<\/a><\/span>!K!/;
+		}
+		elsif($kan_str =~ /\\ecrlink\{(.*?)\}\{(.*?)\}/)
+		{
+			$word = $1;
+			$id = get_index($word);
+			
+			$kan_str =~ s/\\ecrlink\{(.*?)\}\{(.*?)\}/!E!<span class="crossref"><a href="#$id">\2<\/a><\/span>!K!/;
+		}
+		elsif($kan_str =~ /\\ecrref\{kandict_[a-z]\.pdf\}\{[A-Z]\}\{(.*?)\}\{(.*?)\}/)
+		{
+			$kan_str =~ s/\\ecrref\{kandict_[a-z]\.pdf\}\{[A-Z]\}\{(.*?)\}\{(.*?)\}/!E!<span class="crossref"><a href="#">\2<\/a><\/span>!K!/g;
+		}
+		#~ elsif($kan_str =~ /\$(.*?)\$/)
+		#~ {
+			#~ $kan_str =~ s/\$(.*?)\$/!E! \1 !K!/;
+			#~ $kan_str =~ s/\^\\circ/&#xB0;/g;
+		#~ }
+		else
+		{
+			$flag = 0;
+		}
+	}
+
+	$kan_str =~ s/\{//g;
+	$kan_str =~ s/\}//g;
+
+	
+	#print $kan_str . "\n";
+	
+	#$endash = "&#x2014";
+	#$endash_uni = chr(hex($endash));
+	
+	print TMP $kan_str;
+	close(TMP);
+	
+	system("./tmp.o tmp.txt > tmp1.txt");
+	open(UN, "tmp1.txt") or die "Can't open tmp1.txt\n";	
+	my $uni_str = <UN>;
+	close(UN);
+	
+	#print FOUT $uni_str . "\n";
+	
+	
+	
+	my($decval,$val,$p);
+	$uni_str =~ s/<br>//g;
+	$uni_str =~ s/<\/br>//g;
+	$uni_str =~ s/---/&#x2014;/g;
+	$uni_str =~ s/--/&#x2013;/g;
+	$uni_str =~ s/\|/&#x007C;/g;
+	$uni_str =~ s/``/&#x201C;/g;	
+	$uni_str =~ s/''/&#x201D;/g;
+	$uni_str =~ s/`/&#x2018;/g;
+	$uni_str =~ s/'/&#x2019;/g;
+	$uni_str =~ s/&nbsp;/&#xa0;/g;
+	#$uni_str =~ s/(&#x0CCD;)(&#x200C;)(&#x0C97;)(&#x0CCD;)/\1\3\4/;
+		
+	while($flg)
+	{
+		if($uni_str =~ /&#x([0-9A-F]+);/)
+		{
+			$val = $1;	
+			$p = chr(hex($val));
+			$uni_str =~ s/&#x$val;/$p/g;
+		}
+		else
+		{
+			$flg = 0;
+		}
+	}	
+
+	#$uni_str =~ s/\bಸರ್‍\b/ಸರ್/g;
+	
+	return $uni_str;
+}
+
+sub get_index()
+{
+	my($word) = @_;
+	my($i);
+	
+	for($i=0;$i<@indexlist;$i++)
+	{
+		if($indexlist[$i] =~ /\{$word\}/)
+		{
+			return "xid" . ($i+1);
+		}
+	}
+	
+}
+
+
+sub gen_png()
+{
+	my($pron) = @_;
+	
+	open(FOUT,">tmp1.tex") or die "Can' open tmp1.tex";
+	
+	print FOUT '\documentclass{article}';
+	print FOUT '\usepackage{kanlel}';
+	print FOUT '\usepackage[active]{preview}';
+	print FOUT '\begin{document}';
+	print FOUT '\begin{preview}';
+	print FOUT '{\bf ' . $pron . '}';
+	print FOUT '\end{preview}';
+	print FOUT '\end{document}';
+	
+	close(FOUT);
+	
+	system("latex tmp1.tex ;  dvipng -T tight -D 144.54 -o tmp1.png tmp1.dvi ; mv tmp1.png X/pronunciation/xid$xid.png");
+	
+	
+}
 
 sub preprocess()
 {
@@ -757,127 +1072,4 @@ $line =~ s/\\Latin\\ /\\eng\{Latin\} /g;
 $line =~ s/\\Latin/\\eng\{Latin\}/g;
 
 return $line;
-}
-
-
-sub gen_unicode()
-{	
-	my($kan_str) = @_;
-	open(TMP, ">tmp.txt") or die "Can't open tmp.txt\n";
-	my ($tmp,$flg,$i,$endash_uni,$endash,$flag);
-	$flg = 1;
-
-	$kan_str =~ s/\\dictmeaning\{/!E!\\dictmeaning\{!K!/g;
-	$kan_str =~ s/\\num\{(.*?)\}/!E!\\num\{\1\}!K!/g;
-	$kan_str =~ s/\\ralign\{(.*?)\}/!E! $ralign_btag !K! $1 !E! $ralign_etag !K! /g;
-	$kan_str =~ s/\\char'263/!E!&#x0CBD;!K!/g;
-	$kan_str =~ s/\\char'365/!E!&#x0CC4;!K!/g;
-	$kan_str =~ s/\\char'273/!E!&#x0CB1;!K!/g;
-	$kan_str =~ s/\\s /!E!&#x0CBD;!K!/g;
-	$kan_str =~ s/RV/VR/g;
-	$kan_str =~ s/qq/q/g;
-	$kan_str =~ s/Ryx/yxR/g;
-	$kan_str =~ s/RyX/yxR/g;
-	$kan_str =~ s/Rq/qR/g;
-	$kan_str =~ s/RY/YR/g;
-	$kan_str =~ s/\\cdots/!E!&#x2026;!K!/g;
-
-
-	$flag = 1;
-	while($flag)
-	{
-		#print "HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH\n";
-		if($kan_str =~ /\{\\rm (.*?)\}/)
-		{
-			$kan_str =~ s/\{\\rm (.*?)\}/!E!\1!K!/;
-		}
-		elsif($kan_str =~ /\\eng\{(.*?)\}/)
-		{
-			$kan_str =~ s/\\eng\{(.*?)\}/!E!\1!K!/;
-		}
-		#~ elsif($kan_str =~ /\$(.*?)\$/)
-		#~ {
-			#~ $kan_str =~ s/\$(.*?)\$/!E! \1 !K!/;
-			#~ $kan_str =~ s/\^\\circ/&#xB0;/g;
-		#~ }
-		else
-		{
-			$flag = 0;
-		}
-	}
-
-	$kan_str =~ s/\{//g;
-	$kan_str =~ s/\}//g;
-
-	
-	#print $kan_str . "\n";
-	
-	#$endash = "&#x2014";
-	#$endash_uni = chr(hex($endash));
-	
-	print TMP $kan_str;
-	close(TMP);
-	
-	system("./tmp.o tmp.txt > tmp1.txt");
-	open(UN, "tmp1.txt") or die "Can't open tmp1.txt\n";	
-	my $uni_str = <UN>;
-	close(UN);
-	
-	#print FOUT $uni_str . "\n";
-	
-	
-	
-	my($decval,$val,$p);
-	$uni_str =~ s/<br>//g;
-	$uni_str =~ s/<\/br>//g;
-	$uni_str =~ s/---/&#x2014;/g;
-	$uni_str =~ s/--/&#x2013;/g;
-	$uni_str =~ s/\|/&#x007C;/g;
-	$uni_str =~ s/``/&#x201C;/g;	
-	$uni_str =~ s/''/&#x201D;/g;
-	$uni_str =~ s/`/&#x2018;/g;
-	$uni_str =~ s/'/&#x2019;/g;
-	$uni_str =~ s/&nbsp;/&#xa0;/g;
-	#$uni_str =~ s/(&#x0CCD;)(&#x200C;)(&#x0C97;)(&#x0CCD;)/\1\3\4/;
-
-	
-		
-	while($flg)
-	{
-		if($uni_str =~ /&#x([0-9A-F]+);/)
-		{
-			$val = $1;	
-			$p = chr(hex($val));
-			$uni_str =~ s/&#x$val;/$p/g;
-		}
-		else
-		{
-			$flg = 0;
-		}
-	}	
-
-	#$uni_str =~ s/\bಸರ್‍\b/ಸರ್/g;
-	return $uni_str;
-}
-
-sub gen_png()
-{
-	my($pron) = @_;
-	
-	open(FOUT,">tmp1.tex") or die "Can' open tmp1.tex";
-	
-	print FOUT '\documentclass{article}';
-	print FOUT '\usepackage{kanlel}';
-	print FOUT '\usepackage[active]{preview}';
-	print FOUT '\begin{document}';
-	print FOUT '\begin{preview}';
-	print FOUT '{\bf ' . $pron . '}';
-	print FOUT '\end{preview}';
-	print FOUT '\end{document}';
-	
-	close(FOUT);
-	
-	system("latex tmp1.tex ;  dvipng -T tight -D 144.54 -o tmp1.png tmp1.dvi ; mv tmp1.png X/pronunciation/xid$xid.png");
-	
-	
 }
