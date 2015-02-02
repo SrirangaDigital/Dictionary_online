346930f981aef5b4d04117ab359e45a1fa9bb55a
shivasdst <shivably04sdst@gmail.com>
2015-02-02 13:06:31 +0530

    Commit No. 38 - Added letter E and corresponding tex files and images(main and thumbs)

--- src/resize.sh
+++ src/resize.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 c=0
-for i in $(ls D/Pictures/main/*.jpg); 
+for i in $(ls E/Pictures/main/*.jpg); 
 	do 
 		#~ c=`expr $c + 1`;
 		#~ result=`expr $c % 3`
 		jpgfilename=`basename $i`
 		echo $jpgfilename
-		convert -resize 40x $i D/Pictures/thumbs/$jpgfilename; 
+		convert -resize 40x $i E/Pictures/thumbs/$jpgfilename; 
 	done
 
 
