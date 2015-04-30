49644bb005f47a2158bf1d5d46246fd4052611b1
shivasdst <shivably04sdst@gmail.com>
2015-04-30 17:15:27 +0530

    Updated A and other files

--- js/fixhref.js
+++ js/fixhref.js
@@ -15,5 +15,22 @@ var headerHeight = $("header").height();
             window.location.hash = target ;
         });
 	});
+
+  $(".math").each(function(){
+    var texTxt = $(this).text();
+    el = $(this).get(0);
+    if(el.tagName == "DIV"){
+        addDisp = "\\displaystyle";
+    } else {
+        addDisp = "";
+    }
+    try {
+        katex.render(addDisp+texTxt, el);
+    }
+    catch(err) {
+        $(this).html("<span class='err'>"+err);
+    }
+  });
+
 });
 
